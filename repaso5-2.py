2c7850db03127adb8352762b3e8e6a24b867f7cf
R10mar <rafita10martinez@gmail.com>
2023-01-31 12:37:11 +0100

    repaso

---
+++ repaso5-2.py
@@ -0,0 +1,25 @@
+a = int(input("Ingrese un número: "))
+if a == 1 :
+    print("enero")
+elif a == 2:
+    print("febreo")
+elif a == 3:
+    print("marzo") 
+elif a == 4:
+    print("abril") 
+elif a == 5:
+    print("mayo") 
+elif a == 6:
+    print("junio") 
+elif a == 7:
+    print("julio") 
+elif a == 8:
+    print("agosto") 
+elif a == 9:
+    print("septiembre") 
+elif a == 10:
+    print("octubre") 
+elif a == 11:
+    print("noviembre") 
+elif a == 12:
+    print("diciembre")  
