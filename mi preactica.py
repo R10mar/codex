b7fa365c697a711d58cdbadad6a5869713d413f1
R10mar <rafita10martinez@gmail.com>
2023-02-08 09:19:20 +0100

    repaso

---
+++ mi preactica.py
@@ -0,0 +1,14 @@
+print("estas en una jungla y ves un león,¿que haces?")
+print("1 ) saltar al río")
+print("2 ) quedarte quieto")
+respuesta = int(input("1  o  2    "))
+if respuesta == 1:
+    print("te salvaste, subiendote a un barco")
+
+elif respuesta == 2:
+    print("te salvaste porque el león no existe (suertudo)")
+else:
+    print("por no poner lo que era has muerto")
+
+    
+
